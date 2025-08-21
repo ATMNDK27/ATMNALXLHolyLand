68c14135fa8575c960b9862739fb8daaf62b96a6
ATMNDK27 <139294817+atmndk27@users.noreply.github.com>
2025-08-21 23:25:27 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AtmnAlXLApplication"
+include(":app")
+include(":util")
