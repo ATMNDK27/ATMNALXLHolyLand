214748af8b136dc71a238ebc0f3714303e074bfb
苏鹏 <ATMN27963@163.com>
2025-09-16 20:42:26 +0800

    代码清理

--- util/src/main/java/com/example/util/ToastUtil.kt
+++ util/src/main/java/com/example/util/ToastUtil.kt
@@ -5,8 +5,8 @@ import android.widget.Toast
 import androidx.annotation.StringRes
 
 // 获取全局 Application Context（安全）
-val Context.appContext: Context
-    get() = this.applicationContext
+//val Context.appContext: Context
+//    get() = this.applicationContext
 //  扩展函数：让任意 Context 都能直接 toast
 fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
